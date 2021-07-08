776e2a407f645e989aaeb01d46b0c986ea707f2b
jcolon-1993 <60831920+jcolon-1993@users.noreply.github.com>
2021-07-07 18:59:55 -0700

    New Commit

---
+++ js/number-polyfill.js
@@ -0,0 +1,9 @@
+yepnope(
+  {
+    test: Modernizr.inputtypes.number,
+    nope: ["js/numPolyfill.js", "css/number.css"],
+    complete: function()
+    {
+      console.log("YepNope + Modernizr are done");
+    }
+  });
